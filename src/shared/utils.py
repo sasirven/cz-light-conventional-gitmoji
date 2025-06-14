a3c588d81de50df042b4c15163713c20c93f509b
sasirven <77392535+sasirven@users.noreply.github.com>
2025-06-14 15:24:40 +0200

    fix(mojify): 🐛 fix the regex pattern

--- src/shared/utils.py
+++ src/shared/utils.py
@@ -6,18 +6,23 @@ from shared.model import Gitmoji
 from shared.spec import mojis
 
 # global pattern to validate commit messages
-PATTERN = (
-    # To explicitly make . match new line
+BASE_PATTERN = (
     r"(?s)"
-    #  type
     r"^({type})"
-    # scope
     r"(?:\((\S+)\))?!?: "
-    # gitmoji and subject
-    r"(({icon}) (?![\sA-Z])([^\n\r]+)(?<!\s))"
-    # body
+    r"{gitmoji_part}"
     r"(?:\r?\n\r?\n(?!\s)(.+)(?<!\s))?$"
 )
+
+# Gitmoji required
+GITMOJI_REQUIRED = r"(({icon}) (?![\sA-Z])([^\n\r]+)(?<!\s))"
+
+# Gitmoji optional (the whole group is optional, subject still required)
+GITMOJI_OPTIONAL = r"((?:{icon} )?(?![\sA-Z])([^\n\r]+)(?<!\s))"
+
+# Usage:
+PATTERN_REQUIRED = BASE_PATTERN.replace("{gitmoji_part}", GITMOJI_REQUIRED)
+PATTERN_OPTIONAL = BASE_PATTERN.replace("{gitmoji_part}", GITMOJI_OPTIONAL)
 
 
 def get_gitmojis() -> list[Gitmoji]:
@@ -35,11 +40,20 @@ def get_icon_pattern() -> str:
     return "|".join([moji.icon for moji in get_gitmojis()])
 
 
-def get_pattern() -> str:
-    """Return the complete validation pattern."""
+def _base_get_pattern(pattern: LiteralString) -> str:
     type_pattern = get_type_pattern()
     icon_pattern = get_icon_pattern()
 
-    return PATTERN.replace("{type}", type_pattern).replace(
+    return pattern.replace("{type}", type_pattern).replace(
         "{icon}", icon_pattern
     )
+
+
+def get_pattern() -> str:
+    """Return the complete validation pattern."""
+    return _base_get_pattern(PATTERN_REQUIRED)
+
+
+def get_pattern_mojify() -> str:
+    """Return the complete validation pattern for mojify."""
+    return _base_get_pattern(PATTERN_OPTIONAL)
